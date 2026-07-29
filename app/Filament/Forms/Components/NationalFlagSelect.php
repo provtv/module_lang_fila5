93c0ab0b9961a458b4915289ffb694098815a3e1
marco76tv <marco.sottana@gmail.com>
2026-07-29 12:15:35 +0200

    chore: stage NationalFlagSelect dirty state from merge

--- app/Filament/Forms/Components/NationalFlagSelect.php
+++ app/Filament/Forms/Components/NationalFlagSelect.php
@@ -75,21 +75,12 @@ class NationalFlagSelect extends Select
     /**
      * Get filtered country options based on search query.
      *
-<<<<<<< HEAD
      * @param  string  $search  The search query
-=======
-     * @param string $search The search query
-     *
->>>>>>> provtv/dev
      * @return array<string, string>
      */
     protected function getFilteredCountryOptions(string $search): array
     {
-<<<<<<< HEAD
         if (trim($search) === '') {
-=======
-        if ('' === trim($search)) {
->>>>>>> provtv/dev
             return $this->getCountryOptions();
         }
 
