516bee454d9d775542f6d3769a9bb1cea1718c37
marco76tv <marco.sottana@gmail.com>
2026-08-18 12:54:45 +0200

    .

--- app/Filament/Resources/TranslationFileResource.php
+++ app/Filament/Resources/TranslationFileResource.php
@@ -29,41 +29,9 @@ class TranslationFileResource extends XotBaseResource
     }
 
     #[\Override]
-    public static function getFormSchema(): array
+    public static function getFormSchemaOld(): array
     {
         return [];
-
-        /*
-         * return [
-         * Components\TextInput::make('key')
-         * ->required()
-         * ->maxLength(255)
-         * ->disabled()
-         * ->label('Chiave File'),
-         *
-         * Components\TextInput::make('name')
-         * ->required()
-         * ->maxLength(255)
-         * ->disabled()
-         * ->label('Nome File'),
-         *
-         * Components\TextInput::make('path')
-         * ->required()
-         * ->maxLength(255)
-         * ->disabled()
-         * ->label('Percorso File'),
-         *
-         * Components\KeyValue::make('content')
-         * ->label('Traduzioni')
-         * ->keyLabel('Chiave')
-         * ->valueLabel('Valore')
-         * ->addActionLabel('Aggiungi Traduzione')
-         * ->deleteActionLabel('Rimuovi')
-         * ->reorderable()
-         * ->columnSpanFull(),
-         *
-         * ];
-         */
     }
 
     #[\Override]
